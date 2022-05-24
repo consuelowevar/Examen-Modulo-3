7d510f2cc10b341e7397a63016f4ba2e6c90fa98
Consuelo <consue_1990@hotmail.com>
2022-05-23 23:31:08 -0400

    corrección de funcionalidad en

--- app-2.js
+++ app-2.js
@@ -1,126 +1,110 @@
-let colors = 
-    [
-    'rgb(244, 208, 63)', //amarillo --> index/posicion 0
-    'rgb(88, 214, 141)', //verde --> index/ posicion 1
-    'rgb(250, 219, 216)', //rosa --> index 2
-    'rgb(174, 214, 241)', //azul --> index 3
-    'rgb(187, 143, 206)', //lila --> index 4
-    'rgb(247, 249, 249)']; //gris --> index 5
+// let colors = 
+//     [
+//     'rgb(244, 208, 63)', //amarillo --> index/posicion 0
+//     'rgb(88, 214, 141)', //verde --> index/ posicion 1
+//     'rgb(250, 219, 216)', //rosa --> index 2
+//     'rgb(174, 214, 241)', //azul --> index 3
+//     'rgb(187, 143, 206)', //lila --> index 4
+//     'rgb(247, 249, 249)'] //gris --> index 5
 
 
-var square = document.querySelectorAll('.square');
-var textColorRGB = document.getElementById('colorDisplay');
-var pickedColor = pickColor()//colors [0]; //'rgb(244, 208, 63)'  Más adelante, la Variable tendrá un color aleatorio (seleccionado del Arreglo)
+var square = document.querySelectorAll('.square')
+var textColorRGB = document.querySelector('#colorDisplay');
+var pickedColor //= pickColor() 
 var h1 = document.querySelector('h1');
-console.log('pickedColor ' + pickedColor)
+var btnReset = document.getElementById('reset');
+var btnEasy = document.getElementById('easy');
+var btnHard = document.getElementById('hard');
+var numberOfSquares = 6; //INICIA EN 6 SQUARES
 
 
-for (var iterador = 0; iterador < square.length ; iterador++) { //recorre array square y asigna un color
-        square[iterador].style.backgroundColor = colors[iterador];
- }
+function generateRandomColors(numberOfSquares) {
+//                            ^cantidad de squares 
+    for (let j = 0; j < numberOfSquares; j++) {
+        console.log (square[j].style.background = randomColor());
+    }
+}
+generateRandomColors(numberOfSquares) //ejecucion al iniciar la pagina en 6 squares con colores random
 
-textColorRGB.textContent = pickedColor//randomColor(); // textColor es igual al RGB randomizado probar con innerHTML
-// pickedColor = textColorRGB.textContent;
-console.log(pickedColor)
-console.log(textColorRGB.textContent)
 
+//evento click para cada square, donde se compara
+for (let i = 0; i < square.length; i++) {
+    square[i].addEventListener('click', function () {
+        var clickedColor = square[i].style.background
 
- // pinta todos los squares del color ganador
-function changeColors(unColor) {
-    for (var iterador = 0; iterador < square.length ; iterador++ ) {
-        square[iterador].style.backgroundColor = unColor; //textColorRGB.
-    }  
+        if (clickedColor !== textColorRGB.innerHTML) {
+            square[i].style.background = '#232323';
+            message.innerHTML = 'Intentalo Nuevamente';
+
+        } else {
+            message.innerText = '¡Correcto!';
+            h1.style.background = textColorRGB.innerHTML;
+            changeColors(textColorRGB.innerHTML);
+            btnReset.innerHTML = 'Play Again!'
+        }
+    })
 }
 
-//asignacion de color aleatorio del array colors
-function pickColor(params) {
 
-    var indexRandomColor
-    indexRandomColor = Math.floor(Math.random() * square.length) 
-
-    pickedColor = colors[indexRandomColor]; 
-    return pickedColor //colors[index]
-
-    // return  colors[indexRandomColor]
-
-    // console.log(colors[indexRandomColor])
-    // indexRandomColor
-    // console.log(indexRandomColor)
-    // console.log(square[indexRandomColor])
-    // console.log('resultado ' + indexRandomColor)
+// pinta todos los squares del color ganador
+function changeColors() {
+    for (let h = 0; h < square.length; h++) {
+        square[h].style.background = textColorRGB.innerHTML;
+    }
 }
+
+
+//asignacion de color aleatorio d array 
+function pickColor() {
+    var indexRandomColor = square[(Math.floor(Math.random() * numberOfSquares))].style.background;
+    return indexRandomColor
+}
+textColorRGB.innerHTML = pickColor(); //imprime rgb en encabezado
 
 
 function randomColor(params) {
     var red = Math.floor(Math.random() * 256)
     var green = Math.floor(Math.random() * 256)
     var blue = Math.floor(Math.random() * 256)
-    // console.log('esto es un color rgb random ' + 'rgb('+red+', '+green+', '+blue+')')
-    return 'rgb('+red+', '+green+', '+blue+')'
+    return 'rgb(' + red + ', ' + green + ', ' + blue + ')'
 }
 
-
-var newArrayColors = []
-// colors = []
-function generateRandomColors(numeroArraySquare6) {
-    
-    for (var iterador = 0 ; iterador < square.length ; iterador++) {
-        newArrayColors.push(randomColor())   
-    }
-    //console.log(newArrayColors)
-    return newArrayColors
-}
-generateRandomColors()
-
-console.log('L73 ' + newArrayColors)
-
-
-
-
-var square0 = square[0].addEventListener('click', function () {
-    var clickedColor = colors[0];
-
-    if ( pickedColor !== clickedColor ) {
-        square[0].style.backgroundColor = '#232323';
-        message.textContent = 'Intentalo Nuevamente';
-    
-    } else { 
-        message.innerText = '¡Correcto!';
-        h1.style.backgroundColor = pickedColor;
-        changeColors(pickedColor);
-        // console.log('esto esta imprimiendo  ' +  pickedColor);  
-    }
+/**    FX BOTON RESET          */
+btnReset.addEventListener('click', function () {
+    generateRandomColors(numberOfSquares)
+    textColorRGB.innerHTML = pickColor();
+    h1.style.background = '#232323';
+    btnReset.innerHTML = 'Nuevos Colores';
+    message.innerHTML = '';
 })
 
-var square1 = square[1].addEventListener('click', function () {
-    var clickedColor = colors[1];
-
-    if ( pickedColor !== clickedColor ) {
-        square[1].style.backgroundColor = '#232323';
-        message.textContent = 'Intentalo Nuevamente';
-    
-    } else { 
-        message.innerText = '¡Correcto!';
-        h1.style.backgroundColor = pickedColor;
-        changeColors(pickedColor);
-        // console.log('esto esta imprimiendo  ' +  pickedColor);  
+/**    FX BOTON EASY      */
+btnEasy.addEventListener('click', function () {
+    for (let t = 3; t < square.length; t++) {
+        square[t].style.visibility = 'hidden';
     }
+    h1.style.background = '#232323';
+    numberOfSquares = 3;
+    btnEasy.classList.add('selected');
+    btnHard.classList.remove('selected');
+    generateRandomColors(numberOfSquares);
+    textColorRGB.innerHTML = pickColor();
+    console.log('____________')
+
 })
 
-var square2 = square[2].addEventListener('click', function () {
-    var clickedColor = colors[2];
-
-    if ( pickedColor !== clickedColor ) {
-        square[2].style.backgroundColor = '#232323';
-        message.textContent = 'Intentalo Nuevamente';
-    
-    } else { 
-        message.innerText = '¡Correcto!';
-        h1.style.backgroundColor = pickedColor;
-        changeColors(pickedColor);
-        // console.log('esto esta imprimiendo  ' +  pickedColor);  
+/**    FX BOTON HARD      */
+btnHard.addEventListener('click', function () {
+    for (let m = 0; m < square.length ; m++) {
+        square[m].style.visibility = 'visible';
     }
+    h1.style.background = '#232323'; //refresca background 
+    btnHard.classList.add('selected');
+    btnEasy.classList.remove('selected');
+    numberOfSquares = 6;
+    generateRandomColors(numberOfSquares); //generacion de 6 randoms color
+    textColorRGB.innerHTML = pickColor();
+
 })
 
-
-
+//__________________________
